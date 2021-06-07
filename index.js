68e2f0ec6ea1c391212816d9c73758c9f2073d62
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-06-07 21:49:20 +0900

    bodypaser, postman, 회원가입기능 구현

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 7000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(
@@ -17,4 +25,16 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+  // 회원가입 할때 피룡한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
