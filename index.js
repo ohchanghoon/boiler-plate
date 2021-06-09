a71b498a6f1c2dde1bdcec84a750ffb7ffd829b8
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-06-09 16:58:36 +0900

    로그아웃 기능 구현

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const mongoose = require('mongoose');
-const { User } = require('./models/User');
 const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -82,6 +82,15 @@ app.get('/api/users/auth', auth, (req, res) => {
     lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+  });
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -76,7 +76,7 @@ userSchema.statics.findByToken = function (token, cb) {
   var user = this;
 
   // 토큰을 decode 한다.
-  jwt.verify(token, 'secrettoken', function (err, decoded) {
+  jwt.verify(token, 'secretToken', function (err, decoded) {
     // 유저 아이디를 이용해서 유저를 찾은 다음에
     // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
 
