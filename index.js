a87c96744170f19b76e0b30957a6d352228b130c
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-27 16:41:59 +0900

    Refactor: bodyparser를 express로 대체

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 7000;
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const mongoose = require('mongoose');
@@ -10,10 +9,10 @@ const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // application/json
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
@@ -34,7 +33,7 @@ app.post('/api/users/register', (req, res) => {
   // 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
   user.save((err, user) => {
-    if (err) return res.json({ success: false, err });
+    if (err) return res.status(400).json({ success: false, err });
     return res.status(200).json({
       success: true,
     });
@@ -45,7 +44,7 @@ app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 데이터베이스에 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
-      return res.json({
+      return res.status(400).json({
         loginSuccess: false,
         message: '제공된 이메일에 해당하는 유저가 없습니다.',
       });
@@ -54,7 +53,7 @@ app.post('/api/users/login', (req, res) => {
     // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch)
-        return res.json({
+        return res.status(400).json({
           loginSuccess: false,
           message: '비밀번호가 틀렸습니다.',
         });
@@ -89,7 +88,7 @@ app.get('/api/users/auth', auth, (req, res) => {
 
 app.get('/api/users/logout', auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
-    if (err) return res.json({ success: false, err });
+    if (err) return res.status(400).json({ success: false, err });
     return res.status(200).send({
       success: true,
     });
