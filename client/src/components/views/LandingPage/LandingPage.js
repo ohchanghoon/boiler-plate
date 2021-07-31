b790491b2896ac672b7963be5defb1e54b6e706e
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-08-01 00:54:58 +0900

    [Add] create proxy file to resolves cors issues

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 function LandingPage() {
   useEffect(() => {
-    axios
-      .get('http://localhost:7000/api/hello')
-      .then((response) => console.log(response.data));
+    axios.get('/api/hello').then((response) => console.log(response.data));
   }, []);
 
   return <div>LandingPage 랜딩페이지</div>;
